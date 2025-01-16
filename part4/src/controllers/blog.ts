369afcbd0886b2b7e8764c766df29e91fca684b2
banhmisg9509 <banhmisg9509@gmail.com>
2025-01-16 10:21:42 +0700

    init part4 solution

---
+++ part4/src/controllers/blog.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import Blog from "../models/blog";
+
+const router = Router();
+
+router.get("/", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+router.post("/", (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+export default router;
